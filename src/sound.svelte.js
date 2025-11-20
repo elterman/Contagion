de0678207dbe356b537a34aae9c001b788df5471
Arkady Elterman <elterman@gmail.com>
2025-11-19 19:11:27 -0800

    ,.

--- src/sound.svelte.js
+++ src/sound.svelte.js
@@ -24,7 +24,7 @@ const sprite = {
 };
 
 const howl = new Howl({ src: [sounds], sprite });
-const loop = new Howl({ src: [music], loop: true, volume: 0.15 });
+const loop = new Howl({ src: [music], loop: true, volume: 0.25 });
 
 export const _sound = $state({
     sfx: true,
